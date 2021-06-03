ba1e0839752b94eda783b688120093104bfd45a9
Akhil mon <akhilmon@hotmail.com>
2021-06-03 20:03:51 +0530

    Try catch exmple added

--- EnumSample/Form1.Designer.cs
+++ EnumSample/Form1.Designer.cs
@@ -31,11 +31,12 @@ namespace EnumSample
         {
             this.btnToss = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnToss
             // 
-            this.btnToss.Location = new System.Drawing.Point(261, 151);
+            this.btnToss.Location = new System.Drawing.Point(292, 201);
             this.btnToss.Name = "btnToss";
             this.btnToss.Size = new System.Drawing.Size(75, 23);
             this.btnToss.TabIndex = 0;
@@ -54,16 +55,25 @@ namespace EnumSample
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
             // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(108, 154);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(413, 20);
+            this.txtNumber.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(596, 324);
+            this.Controls.Add(this.txtNumber);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnToss);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +81,7 @@ namespace EnumSample
 
         private System.Windows.Forms.Button btnToss;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtNumber;
     }
 }
 
--- EnumSample/Form1.cs
+++ EnumSample/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,19 +20,49 @@ namespace EnumSample
 
         private void btnToss_Click(object sender, EventArgs e)
         {
-            TossResult tossResult=TossResult.NA;
+            //TossResult tossResult=TossResult.NA;
 
-            if (comboBox1.Text=="Tail")
-            {
-                 tossResult = Tossing(Coin.Tail);
-            }
-            else if (comboBox1.Text == "Head")
-            {
-                 tossResult = Tossing(Coin.Head);
-            }
-            this.Text = tossResult.ToString();
+            //if (comboBox1.Text=="Tail")
+            //{
+            //     tossResult = Tossing(Coin.Tail);
+            //}
+            //else if (comboBox1.Text == "Head")
+            //{
+            //     tossResult = Tossing(Coin.Head);
+            //}
+            //this.Text = tossResult.ToString();
 
+            //try
+            //{
+            //    int a = Convert.ToInt32(txtNumber.Text);
+            //    this.Text = a.ToString();
+            //}
+            //catch (FormatException)
+            //{
+            //    MessageBox.Show("Invalid data enterd");
 
+            //}
+            //catch (OverflowException)
+            //{
+            //    MessageBox.Show("Long data enterd");
+
+            //}
+            //catch (Exception)
+            //{
+            //    MessageBox.Show("Somethis went wrong");
+
+            //}
+
+            //try
+            //{
+            //    this.Text = File.ReadAllText(@"C:\Users\Akhil\Desktop\MyData.txt");
+            //}
+            //catch (FileNotFoundException)
+            //{
+
+            //    MessageBox.Show("File not found");
+            //}
+           
         }
 
 
